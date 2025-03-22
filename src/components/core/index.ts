17bcdab0a6241c7d423193dfaa8893b9996e4a28
Avirup Ghosh <oviirup@gmail.com>
2025-03-22 19:42:07 +0530

    🗑️ removed unused packages & files

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,5 +1,3 @@
-export { default as Image } from 'next/image';
 export { Anchor } from './anchor';
-export { Slot } from './slot';
 export { Icon } from './icon';
 export { Sprite } from './sprite';
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,25 @@
+import { cx } from 'class-variance-authority';
+import { twMerge } from 'tailwind-merge';
 import { SITE_URL } from '~/const';
+import { isFunction, isObject } from '~/lib/assertions';
+
+/** Creates a formatted className from given arguments */
+export function cn(...args: any[]) {
+  return twMerge(cx(args));
+}
+
+type PossibleRef<El> = React.Ref<El> | undefined;
+/** Use multiple refs on a single element */
+export function referrals<El extends Element>(...refs: PossibleRef<El>[]) {
+  if (!refs.length) return;
+  return (el: El) => {
+    for (const ref of refs) {
+      if (!ref) continue;
+      else if (isFunction(ref)) ref(el);
+      else if (isObject(ref)) ref.current = el;
+    }
+  };
+}
 
 /**
  * Returns the canonical url to given path and params
