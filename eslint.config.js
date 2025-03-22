20d56a81b22003c269366764626f77ecb507d200
Avirup Ghosh <oviirup@gmail.com>
2025-03-22 19:44:16 +0530

    📦 using tailwind v4

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,19 @@
-import path from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import config_prettier from 'eslint-config-prettier';
 import plugin_onlyWarn from 'eslint-plugin-only-warn';
-import plugin_tailwind from 'eslint-plugin-tailwindcss';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 /** @type {import('eslint').Linter.Config[]} */
 const config = [
   config_prettier,
   // next.js plugin
-  ...compat.extends('next/core-web-vitals'),
-  ...compat.extends('next/typescript'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
       '@typescript-eslint/ban-ts-comment': 'off',
@@ -27,19 +21,6 @@ const config = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
-    },
-  },
-  // tailwindcss plugin
-  ...plugin_tailwind.configs['flat/recommended'],
-  {
-    rules: {
-      'tailwindcss/no-custom-classname': 'off',
-    },
-    settings: {
-      tailwindcss: {
-        callees: ['clsx', 'cn', 'cva'],
-        classRegex: '^(class|\\w+Class)Name$',
-      },
     },
   },
   // base rules
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import { cn } from '~/components/utils';
+import { cn } from '~/lib/utils';
 import { inter } from '~/styles/fonts';
-import '~/styles/globals.scss';
+import '~/styles/globals.css';
 
 export { metadata, viewport } from './metadata';
 
 export default function RootLayout({ children }: Props) {
   const fontVariables = [inter.style, inter.variable];
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="light">
       <body className={cn(fontVariables, 'flex min-h-svh antialiased')}>{children}</body>
     </html>
   );
