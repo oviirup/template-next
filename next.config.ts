1c66d24ac023bb83579d94e89e9cd49d1746ab9e
oviirup <49820575+oviirup@users.noreply.github.com>
2025-12-30 12:43:30 +0530

    🛠️ update project config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,8 @@
-import { build } from '@oviirup/sprite';
-import { isDev } from '@/config/env';
-import type { NextConfig } from 'next';
-
-// build sprite files once
-if (!process.env.sprite_build_complete) {
-  build({ watch: isDev });
-  process.env.sprite_build_complete = 'TRUE';
-}
+import { NextConfig } from "next";
 
 const config: NextConfig = {
   devIndicators: false,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  logging: {
-    fetches: { fullUrl: true, hmrRefreshes: true },
-  },
-  experimental: {
-    reactCompiler: { compilationMode: 'annotation' },
-    optimizePackageImports: ['lucide-react', 'radix-ui'],
-  },
+  reactCompiler: true,
 };
 
 export default config;
